44f2d161d4ce5d9184d3bce2acff306963fffb54
besonder <besonder722@gmail.com>
2022-12-05 04:06:28 +0900

    .

--- layers.py
+++ layers.py
@@ -44,6 +44,7 @@ class StridedConv(nn.Module):
         if striding:
             self.register_forward_pre_hook(lambda _, x: \
                     einops.rearrange(x[0], downsample, k1=2, k2=2))  
+
         
 def cayley(W, ED=False):
     if len(W.shape) == 2:
@@ -99,24 +100,36 @@ class CayleyConv(StridedConv, nn.Conv2d):
 
 class CayleyConvED(StridedConv, nn.Conv2d):
     def __init__(self, *args, **kwargs):
-        super().__init__(args[0], args[0], args[2], **kwargs)
+        # super().__init__(*args, **kwargs)
+        if 'stride' in kwargs and kwargs['stride'] == 2:
+            self.stride = 2
+            self.xcin = 4*args[0]
+            self.padding = 1
+            self.k = max(1, args[2]//2)
+        else:
+            self.stride = 1
+            self.xcin = args[0]
+            self.padding = 0
+            self.k =args[2]
+        super().__init__(args[0], self.xcin, self.k, padding=self.padding, stride=self.stride)
         self.bias = nn.Parameter(torch.zeros(args[1]))
         self.args = args
         self.register_parameter('alpha', None)
         self.H = None
 
-    def genH(self, n, k, cout, cin):
-        conv = nn.Conv2d(cin, cout, k, bias=False)
+
+    def genH(self, n, k, cout, xcin):
+        conv = nn.Conv2d(xcin, cout, k, bias=False)
         s = (conv.weight.shape[2] - 1) // 2
         shift_matrix = self.fft_shift_matrix(n, -s)[:, :(n//2 + 1)].reshape(n * (n // 2 + 1), 1, 1).to(conv.weight.device)
         optimizer = torch.optim.SGD(conv.parameters(), lr=0.1)
         loss = torch.nn.MSELoss()
         for i in range(100):
-            H = shift_matrix*torch.fft.rfft2(conv.weight, (n, n)).reshape(cout, cin, n * (n//2+1)).permute(2, 0, 1).conj()
-            b, cout, cin = H.shape
+            H = shift_matrix*torch.fft.rfft2(conv.weight, (n, n)).reshape(cout, xcin, n * (n//2+1)).permute(2, 0, 1).conj()
+            b, cout, xcin = H.shape
             Hnorm = torch.norm(H, dim=2)
             
-            L1 = loss(Hnorm, torch.ones_like(Hnorm, dtype=Hnorm.dtype)*np.sqrt(cin/cout))
+            L1 = loss(Hnorm, torch.ones_like(Hnorm, dtype=Hnorm.dtype)*np.sqrt(xcin/cout))
             
             HHnorm = torch.norm(H, dim=1)
             L2 = loss(HHnorm, torch.ones_like(HHnorm, dtype=HHnorm.dtype))
@@ -138,7 +151,7 @@ class CayleyConvED(StridedConv, nn.Conv2d):
             L.backward()
             optimizer.step()
 
-        H = shift_matrix*torch.fft.rfft2(conv.weight, (n, n)).reshape(cout, cin, n * (n//2+1)).permute(2, 0, 1).conj()
+        H = shift_matrix*torch.fft.rfft2(conv.weight, (n, n)).reshape(cout, xcin, n * (n//2+1)).permute(2, 0, 1).conj()
         self.H = H.to(self.weight.device).detach()
 
 
@@ -149,21 +162,21 @@ class CayleyConvED(StridedConv, nn.Conv2d):
 
     
     def forward(self, x):
-        cin = self.args[0]
         cout = self.args[1]
 
         batches, _, n, _ = x.shape
         if not hasattr(self, 'shift_matrix'):
             s = (self.weight.shape[2] - 1) // 2
             self.shift_matrix = self.fft_shift_matrix(n, -s)[:, :(n//2 + 1)].reshape(n * (n // 2 + 1), 1, 1).to(x.device)
-        xfft = torch.fft.rfft2(x).permute(2, 3, 1, 0).reshape(n * (n // 2 + 1), cin, batches)
 
-        wfft = self.shift_matrix * torch.fft.rfft2(self.weight, (n, n)).reshape(cin, cin, n * (n // 2 + 1)).permute(2, 0, 1).conj()
+        xfft = torch.fft.rfft2(x).permute(2, 3, 1, 0).reshape(n * (n // 2 + 1), self.xcin, batches)
+
+        wfft = self.shift_matrix * torch.fft.rfft2(self.weight, (n, n)).reshape(self.xcin, self.xcin, n * (n // 2 + 1)).permute(2, 0, 1).conj()
         if self.alpha is None:
             self.alpha = nn.Parameter(torch.tensor(wfft.norm().item(), requires_grad=True).to(x.device))
 
         if self.H == None:
-            self.genH(n, self.kernel_size[0], cout, cin)
+            self.genH(n, self.kernel_size[0], cout, self.xcin)
 
         cwxfft = self.H @ cayley(self.alpha * wfft / wfft.norm(), ED=True) @ xfft
 
